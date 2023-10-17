4cf61a0a5c17491c6312f48ae9f0e94aba393050
Ubuntu <ubuntu@ip-172-31-45-83.eu-west-1.compute.internal>
2023-10-17 10:53:05 +0000

    gestion des codes retours + 206

--- PE.py
+++ PE.py
@@ -7,6 +7,7 @@ from pprint import pprint
 import sys
 from pymongo import MongoClient
 import pandas as pd
+import json
 
 ID_CLIENT= "PAR_datascientest_98cd282e8a1a1386298c211358d8af06dfa5ab10edb133a40c513453375fcabb"
 KEY= "37a59304f2c51b9cf9009fa731771b578468964e70c10d4782e1a9ae852f7b24"
@@ -15,9 +16,10 @@ KEY= "37a59304f2c51b9cf9009fa731771b578468964e70c10d4782e1a9ae852f7b24"
 API_KEY = os.environ.get("API_KEY")
 print("***********", API_KEY, "*****************")
 
-#----------------------------------------------------
+#############################################################
 # Appel de l'API de génération du Token
-#----------------------------------------------------
+#############################################################
+
 # Paramètre du point d"accès (URL, entête et token)
 url_token = "https://entreprise.pole-emploi.fr/connexion/oauth2/access_token?realm=%2Fpartenaire"
 headers_token = { "Content-Type": "application/x-www-form-urlencoded"}
@@ -35,44 +37,67 @@ if r_token.status_code == 200 :
 else:
     sys.exit(f"Erreur lors de l'appel à l'API de génération du token de connection, code retour {r_token.status_code}")
 
-#----------------------------------------------------
+#############################################################
 # Appel de l'API de récupération des jobs Pôle Emploi
-#----------------------------------------------------
+#############################################################
+
 # Paramètre du point d"accès (URL, autorisation et paramètre)
+seq_appel = 0
 url_req = "https://api.pole-emploi.io/partenaire/offresdemploi/v2/offres/search?"
 authorization = {"Authorization": f"Bearer {access_token_bearer}"}
 params = {
-     range : 0-2 / 1,
+    "range" : "0-149",
     "qualification": "0"  # Niveau de qualification demandé
     #"motsCles": "informatique",  # Recherche par mot clé
     #"commune": "67482",  # Exemple de codes INSEE de communes
 }
-#récupération et gestion de la réponse 
-r_req = requests.get(url_req, params=params, headers=authorization)
-if r_req.status_code == 200 or r_req.status_code == 206:
-    print("Récupération de la liste des jobs de Pôle Emploi : OK")
-else:
-    print(r_req)
-    sys.exit(f"Erreur lors de l'appel à l'API de génération du token de connection, code retour {r_req.status_code}")
+
+
+#Fonctions du programme
+def maj_param_appel():
+    param_range = params["range"].split("-")
+    param_range = [int(i) + 150 for i in param_range]
+    params["range"] = str(param_range[0])+"-"+ str(param_range[1])
     
-#écriture du Json dans un fichier en sortie
+
+def appel_API():
+    r_req = requests.get(url_req, params=params, headers=authorization)
+    return r_req
+
+#----------------------------------
+#traitement principal
+#----------------------------------
+
+#récupération et transormation de la réponse en json
+r_req = appel_API()
 dataPE = r_req.json()["resultats"]
 
+while (seq_appel == 0 or r_req.status_code == 206) and seq_appel < 19:
+    maj_param_appel()
+    r_req = appel_API()
+    seq_appel += 1 
+    dataPE = dataPE + r_req.json()["resultats"]
 
+#gestion des codes retour
+if r_req.status_code == 200:
+    print("Récupération de la liste des jobs de Pôle Emploi : OK")
+elif r_req.status_code == 206 and seq_appel == 19:
+    print("Récupération partielle de la liste des jobs de Pôle Emploi : nombre de jobs dépassant les 3000 !")
+else:
+    print("/",seq_appel,"/")    
+    print("/",r_req.status_code,"/")
+    sys.exit(f"Erreur lors de l'appel à l'API de récupération des jobs, code retour {r_req.status_code}")
+    
 
-import json
 #écriture d'un fichier externe pour visualisation du json de réponse
-with open("dataPE.json", "w") as fichier:
+"""with open("dataPE.json", "w") as fichier:
     json.dump(dataPE, fichier, indent=4)
 with open("r_req.json", "w") as fichier:
-    fichier.write(r_req.text)
+    fichier.write(r_req.text)"""
 
-
-#pprint(r_req.keys())
-###################################################
+#---------------------------------------------------
 # gestion des données issues de l"API
-###################################################
-
+#---------------------------------------------------
 
 # Connexion à MongoDB
 client = MongoClient(host="127.0.0.1", port = 27017)
@@ -84,7 +109,7 @@ c_PE = DB["PE"]
 c_PE.drop()
 
 
-
+#alimentation de la collection c_PE de la DB
 data_collection = []
 for doc in dataPE:
     data_doc = {
@@ -119,18 +144,15 @@ for doc in dataPE:
     "typeContrat": doc.get("typeContrat"),
     "typeContratLibelle": doc.get("typeContratLibelle")
     }
-
-    data_collection.append(data_doc)
+    data_collection.append(data_doc)      
 
 c_PE.insert_many(data_collection)
-
-
-#alimentation de la DB
-
-c_PE.insert_many(dataPE)
-#pprint(c_PE.find_one())
 pprint(c_PE.count_documents({}))
 
+df3 = pd.DataFrame(list(c_PE.find()))["id"].drop_duplicates()
+print("réel :", df3.value_counts().sum())
+#print dans un fichier
+#df3.to_csv('df3.csv', index=False)
 
 
 cle_dataPE = []
@@ -141,8 +163,6 @@ for i in dataPE:
 df = pd.DataFrame({"cle" : cle_dataPE})
 # on supprime les duplicates keys
 df2 = df["cle"].drop_duplicates().tolist()
-
-
 pprint(df2) 
 
 
